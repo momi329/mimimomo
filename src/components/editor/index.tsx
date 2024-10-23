a46395fc4cbc9e7e7af22446ab9c9d493ff51517
momi329 <amekaka@gmail.com>
2024-10-23 23:51:28 +0800

    feat: add drag create block

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -3,8 +3,17 @@
 import Display from "@/components/editor/display";
 import Editor from "@/components/editor/editor";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_IMAGE_DATA, DEFAULT_TEXT_DATA } from "@/data/editor-data";
+import { cn } from "@/lib/utils";
 import { Data } from "@/types/editor.type";
 import { useEffect, useRef, useState } from "react";
+
+const createNewData = (type: Data["type"]) => {
+  const id = `${type}-${Date.now()}`;
+  if (type === "image") return { id, ...DEFAULT_IMAGE_DATA };
+  if (type === "text") return { id, ...DEFAULT_TEXT_DATA };
+  return null;
+};
 
 type InterviewEditorProps = {
   defaultData: Data[];
@@ -15,6 +24,9 @@ const InterviewEditor = ({ defaultData }: InterviewEditorProps) => {
 
   const [dataList, setDataList] = useState<Data[]>(defaultData);
   const [editing, setEditing] = useState<number | null>(null);
+
+  const [dataType, setDataType] = useState<Data["type"] | null>(null);
+  const [destinationIndex, setDestinationIndex] = useState<number | null>(null);
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
@@ -42,19 +54,60 @@ const InterviewEditor = ({ defaultData }: InterviewEditorProps) => {
     setEditing(index);
   };
 
+  const handleDragStart = (type: Data["type"]) => () => {
+    setDataType(type);
+  };
+
+  const handleDragEnd = (e: React.DragEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setDataType(null);
+    setDestinationIndex(null);
+  };
+
+  const handleDrop =
+    (index: number) => (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      if (dataType === null) return;
+      setDataList((prev) => {
+        const dataBefore = prev.slice(0, index);
+        const dataAfter = prev.slice(index);
+        const newDataList = [
+          ...dataBefore,
+          createNewData(dataType)!,
+          ...dataAfter,
+        ];
+        console.log(newDataList);
+        return newDataList;
+      });
+    };
+
+  const handleDragOver =
+    (index: number) => (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      setDestinationIndex(index);
+    };
+
   return (
     <div className="flex h-screen">
-      <div
-        className="bg-slate-100 w-1/3 border-r border-g ray-200 flex flex-col gap-4 p-4"
-        onClick={(e) => {
-          console.log("clicked");
-          e.stopPropagation();
-        }}
-      >
+      <div className="bg-slate-100 w-1/3 border-r border-g ray-200 flex flex-col gap-4 p-4">
         {editing === null ? (
           <>
-            <Button className="w-20">圖片</Button>
-            <Button className="w-20">文字</Button>
+            <Button
+              className="w-20"
+              draggable
+              onDragStart={handleDragStart("image")}
+              onDragEnd={handleDragEnd}
+            >
+              圖片
+            </Button>
+            <Button
+              className="w-20"
+              draggable
+              onDragStart={handleDragStart("text")}
+              onDragEnd={handleDragEnd}
+            >
+              文字
+            </Button>
           </>
         ) : (
           <div ref={editorRef} className="grid gap-4">
@@ -62,13 +115,16 @@ const InterviewEditor = ({ defaultData }: InterviewEditorProps) => {
           </div>
         )}
       </div>
-      <div className="w-2/3">
+      <div className="w-2/3 flex flex-col gap-1">
         {dataList.map((item, index) => (
-          <Display
+          <div
             key={item.id}
-            data={item}
-            onClick={() => handleClick(index)}
-          />
+            onDrop={handleDrop(index)}
+            onDragOver={handleDragOver(index)}
+            className={cn(destinationIndex === index && "bg-slate-200")}
+          >
+            <Display data={item} onClick={() => handleClick(index)} />
+          </div>
         ))}
       </div>
     </div>
--- src/data/editor-data.ts
+++ src/data/editor-data.ts
@@ -3,13 +3,19 @@ import { Data } from "@/types/editor.type";
 export const DEFAULT_IMAGE_URL =
   "https://i.pinimg.com/enabled_hi/564x/6e/6f/f6/6e6ff6214eccb3999827f39382f83712.jpg";
 
+export const DEFAULT_IMAGE_DATA = {
+  type: "image",
+  src: DEFAULT_IMAGE_URL,
+  width: "200px",
+  height: "200px",
+} as const;
+
+export const DEFAULT_TEXT_DATA = {
+  type: "text",
+  text: "wellll",
+} as const;
+
 export const EDITOR_DATA: Data[] = [
-  {
-    id: "image-1",
-    type: "image",
-    src: DEFAULT_IMAGE_URL,
-    width: "200px",
-    height: "200px",
-  },
-  { id: "text-1", type: "text", text: "wellll" },
+  { id: "image-1", ...DEFAULT_IMAGE_DATA },
+  { id: "text-1", ...DEFAULT_TEXT_DATA },
 ];

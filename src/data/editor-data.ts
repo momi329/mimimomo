4cb5462fa8840b1a93784c8ab31e797fd840d664
momi329 <amekaka@gmail.com>
2024-10-23 21:58:34 +0800

    feat: extract default data

---
+++ src/data/editor-data.ts
@@ -0,0 +1,10 @@
+import { Data } from "@/types/editor.type";
+
+export const EDITOR_DATA: Data[] = [
+  {
+    id: "image-1",
+    type: "image",
+    src: "https://i.pinimg.com/enabled_hi/564x/6e/6f/f6/6e6ff6214eccb3999827f39382f83712.jpg",
+  },
+  { id: "text-1", type: "text", text: "wellll" },
+];

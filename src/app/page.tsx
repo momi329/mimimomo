2f09e5f9ce515f9865107567ba85e68354316aac
momi329 <amekaka@gmail.com>
2024-10-25 13:11:38 +0800

    fix: package problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="">index page</div>;
+  return (
+    <div className="text-3xl p-20">
+      Link to{" "}
+      <Link className="cursor-pointer underline text-pink-400" href="/editor">
+        Editor
+      </Link>
+    </div>
+  );
 }
